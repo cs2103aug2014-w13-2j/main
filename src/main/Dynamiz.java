753d844651fcf0bb1f763f3d1c2c6d5547b7137c
ZiXian92 <zixian1992@hotmail.com>
2014-08-27 21:08:21 +0800

    Creates working class file of main program

---
+++ src/main/Dynamiz.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author zixian
+ *
+ */
+
+/*
+ * Defines the object class of the main program.
+ */
+public class Dynamiz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
