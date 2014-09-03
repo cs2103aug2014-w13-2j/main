d59aacc28784698d6f1a4118424c328b26ce43d1
Vio8023 <Vio@r-198-176-28-172.comp.nus.edu.sg>
2014-09-03 17:28:59 +0800

    Display class added

---
+++ src/main/Display.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Display {
+	private static final ;
+	
+	public static void taskDisplay(ArrayList<TaskItem> taskList){
+		
+	}
+	
+	public static void taskDisplay(TaskItem thisTask){
+		
+	}
+	public static void stringDisplay(ArrayList<String> strings){
+		
+	}
+	public static void stringDisplay(String thisString){
+		
+	}
+	public static void welcomeMessage(){
+		
+	}
+}
