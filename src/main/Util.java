50eab4319f49a7d8807d8498ea41b24b20494fa9
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-09-04 10:46:40 +0800

    Added a Util class to modify string. Helping in Parser

---
+++ src/main/Util.java
@@ -0,0 +1,54 @@
+package main;
+
+public final class Util {
+	
+	/**
+	 * Remove the hyphens (single or double) from the given str and
+	 * return a new String
+	 * 
+	 * @param str The string from which the hyphens should be removed.
+	 * 
+	 * @return the new stripped String.
+	 */
+	public static String stripLeadingHyphens(String str) {
+		if (str == null) {
+			return null;
+		}
+		
+		if (str.startsWith("--")) {
+			return str.substring(2, str.length()); 
+		} else if (str.startsWith("-")) {
+			return str.substring(1, str.length());
+		} else {
+			// Do nothing here
+		}
+		
+		return str;
+	}
+	
+	/**
+	 * Remove the leading and ending quotes from the string and 
+	 * return a new String
+	 * 
+	 * For example, '"Hello World"' should returns 'Hello World'
+	 * 
+	 * @param str The string from which the quotes should be removed.
+	 * 
+	 * @return the string without leading and trailing quotes.
+	 */
+	public static String stripLeadingAndTrailingQuotes(String str) {
+		int length = str.length();
+		
+		boolean hasSufficientLength = str.length() > 1;
+		boolean hasLeadingAndTrailingQuotes = str.startsWith("\"") && 
+											  str.endsWith("\"");
+		// Index -1 means not found.
+		boolean hasNoMiddleQuotes = str.substring(1, length - 1).indexOf("\"") == -1;
+		
+		if (hasSufficientLength && hasLeadingAndTrailingQuotes && hasNoMiddleQuotes) {
+			str = str.substring(1, length - 1);
+		}
+		
+		return str;
+	}
+}
