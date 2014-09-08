9ce936ec7f05d0ced39717b383dbcf92053767f9
Vio8023 <Vio@r-69-176-28-172.comp.nus.edu.sg>
2014-09-08 13:23:34 +0800

    Exception

--- src/main/Display.java
+++ src/main/Display.java
@@ -1,9 +1,9 @@
 package main;
 
 import java.util.ArrayList;
-
+import java.awt.color.*;
 public class Display {
-	private static final ;
+	private static final String WELCOME_MESSAGE= "Welcome to Dynamiz!";
 	
 	public static void taskDisplay(ArrayList<TaskItem> taskList){
 		
@@ -13,10 +13,12 @@ public class Display {
 		
 	}
 	public static void stringDisplay(ArrayList<String> strings){
-		
+		for(int i=0;i<strings.size();i++){
+			System.out.println(strings.get(i));
+		}
 	}
 	public static void stringDisplay(String thisString){
-		
+		System.out.println(thisString);
 	}
 	public static void welcomeMessage(){
 		
