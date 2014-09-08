ba7ae599f354b5f44add17841e1d89153fabeb52
Vio8023 <Vio@166-230.priv23.nus.edu.sg>
2014-09-08 21:25:40 +0800

    Display Update
    New added Formatter;

--- src/main/Display.java
+++ src/main/Display.java
@@ -1,26 +1,54 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Formatter;
 import java.awt.color.*;
+
+/**
+ * @author Hu Wenyan
+ *
+ */
 public class Display {
 	private static final String WELCOME_MESSAGE= "Welcome to Dynamiz!";
-	
+
+	public static void printWelcomeMessage(){
+		System.out.println(WELCOME_MESSAGE);
+	}
 	public static void taskDisplay(ArrayList<TaskItem> taskList){
-		
+		if(taskList==null) return;
+		for(int i=0;i<taskList.size();i++){
+			taskDisplay(taskList.get(i));
+		}
 	}
 	
 	public static void taskDisplay(TaskItem thisTask){
-		
+		String description=thisTask.getDescription();
+		String startTime=thisTask.getStartDate();
+		String endTime=thisTask.getEndDate();
+		String priority=thisTask.getPriority();
+		String status=thisTask.getStatus();
+		ArrayList<String> categories=thisTask.getCategory();
+		Formatter taskFormatter = new Formatter();
+		taskFormatter.format(description,startTime);
+		System.out.print(taskFormatter);
+	}
+	private String dateFormatter(Calendar c){
+		String s = String.format("%1$tm,%1$te",c);
+		return s;	
+	}
+	private String timeFormatter(Date d){
+		String s = String.format("%tH:%tM", d);
+		return s;
 	}
 	public static void stringDisplay(ArrayList<String> strings){
 		for(int i=0;i<strings.size();i++){
-			System.out.println(strings.get(i));
+			stringDisplay(strings.get(i));
 		}
 	}
 	public static void stringDisplay(String thisString){
 		System.out.println(thisString);
 	}
-	public static void welcomeMessage(){
-		
-	}
+	
+	
 }
