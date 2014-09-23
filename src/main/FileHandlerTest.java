4c2850b41b13be48b03340f99f23af1879bdd005
ZiXian92 <zixian1992@hotmail.com>
2014-09-23 23:39:28 +0800

    Completes FileHandler class(File I/O)

--- src/main/FileHandlerTest.java
+++ src/main/FileHandlerTest.java
@@ -5,19 +5,27 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class FileHandlerTest {
     
     @Test
     public void testGetListFromFile() {
-	fail("not implemented.");
+	ArrayList<ToDoItem> list = FileHandler.getListFromFile();
+	assertEquals("list has 3 items", 3, list.size());
     }
     
     @Test
     public void testWriteToFile() {
-	fail("not implemented.");
+	try{
+	    ArrayList<ToDoItem> list = FileHandler.getListFromFile();
+	    //FileHandler.writeListToFile(list, "output.txt");
+	    FileHandler.writeListToFile(list);
+	    list = FileHandler.getListFromFile();
+	    assertEquals("list has 3 items", 3, list.size());
+	} catch(IOException ioe){
+	    
+	}
     }
     
 }
