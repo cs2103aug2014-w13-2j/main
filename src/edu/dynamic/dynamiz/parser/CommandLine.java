098567e2f389c2aba65fc255d4043dceef43f53e
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-09-28 15:26:02 +0800

    Set up basic operation for CommandLine

--- src/edu/dynamic/dynamiz/parser/CommandLine.java
+++ src/edu/dynamic/dynamiz/parser/CommandLine.java
@@ -2,8 +2,10 @@ package edu.dynamic.dynamiz.parser;
 
 import java.util.List;
 
+import edu.dynamic.dynamiz.logic.Command;
+
 /**
- * This is a class Object which stores the information of the parsed 
+ * This is a class which stores the information of the parsed 
  * command line given by the user.
  * 
  * Represents a list of arguments parsed against an option argument.
@@ -17,5 +19,64 @@ import java.util.List;
  *
  */
 public class CommandLine {
+	private Command command;
+	private Options options;
+	private String param;
+	
+	public CommandLine() {
+		this.command = null;
+		this.options = null;
+		this.param = null;
+	}
+	
+	public CommandLine(Command command, Options options, String param) {
+		this.command = command;
+		this.options = options;
+		this.param = param;
+	}
 
+	/*
+	 * ========================================================================
+	 * Getters & Setters
+	 * ========================================================================
+	 */
+	public Command getCommand() {
+		return command;
+	}
+
+	public void setCommand(Command command) {
+		this.command = command;
+	}
+
+	public Options getOptions() {
+		return options;
+	}
+
+	public void setOptions(Options options) {
+		this.options = options;
+	}
+
+	public String getParam() {
+		return param;
+	}
+
+	public void setParam(String param) {
+		this.param = param;
+	}
+	/*
+	 * ========================================================================
+	 * End of Getters & Setters
+	 * ========================================================================
+	 * 
+	 */
+	
+	public String getCommandType() {
+		return command.getType();
+	}
+	
+	public int getNumberOfOptions() {
+		return options.getNumOfOptions();
+	}
+	
+	
 }
