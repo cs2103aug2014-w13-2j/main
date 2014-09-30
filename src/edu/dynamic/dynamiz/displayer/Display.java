498391d91c8018410bfae8b3787cb855724c61d8
Vio8023 <Vio@Hus-MacBook-Pro-2.local>
2014-09-30 19:13:30 +0800

    printGetFile and printFeedBack added

--- src/edu/dynamic/dynamiz/displayer/Display.java
+++ src/edu/dynamic/dynamiz/displayer/Display.java
@@ -12,7 +12,7 @@ import edu.dynamic.dynamiz.structure.ToDoItem;
 
 /**
  * @author Hu Wenyan
- *
+ * Implement Display functions for tasks, events and todoItems
  */
 public class Display implements DisplayerInterface {
 	private static final String WELCOME_MESSAGE= "Welcome to Dynamiz!";
@@ -84,52 +84,129 @@ public class Display implements DisplayerInterface {
 		
 	}
 	public void displayTaskItem(TaskItem task) {
-		// TODO Auto-generated method stub
+		System.out.println(task.toString());
 		
 	}
 	
 	
 	public void displayEventList(ArrayList<EventItem> eventList) {
-		// TODO Auto-generated method stub
+		if(eventList==null) return;
+		else{
+			System.out.println(taskListTitle());
+			System.out.println(dividingLine());
+		for(int i=0;i<eventList.size();i++){
+			EventItem thisEvent= eventList.get(i);
+			System.out.println(thisEvent.toString());
+		}
+		}
 		
 	}
 	@Override
 	public void displayEventList(EventItem[] eventList) {
-		// TODO Auto-generated method stub
+		if(eventList==null) return;
+		else{
+			System.out.println(taskListTitle());
+			System.out.println(dividingLine());
+		for(int i=0;i<eventList.length;i++){
+			EventItem thisEvent= eventList[i];
+			System.out.println(thisEvent.toString());
+		}
+		}
 		
 	}
 	@Override
 	public void displayEventItem(EventItem event) {
-		// TODO Auto-generated method stub
+		System.out.println(event.toString());
 		
 	}
 	@Override
 	public void displayToDoList(ArrayList<ToDoItem> todoList) {
-		// TODO Auto-generated method stub
+		if(todoList==null) return;
+		else{
+			
+		for(int i=0;i<todoList.size();i++){
+			ToDoItem thisToDo= todoList.get(i);
+			System.out.println(thisToDo.toString());
+		}
+		}
 		
 	}
 	@Override
 	public void displayToDoList(ToDoItem[] todoList) {
-		// TODO Auto-generated method stub
+		if(todoList==null) return;
+		else{
+			System.out.println(taskListTitle());
+			System.out.println(dividingLine());
+		for(int i=0;i<todoList.length;i++){
+			ToDoItem thisTodo= todoList[i];
+			System.out.println(thisTodo.toString());
+		}
+		}
 		
 	}
 	@Override
 	public void displayToDoItem(ToDoItem todoItem) {
-		// TODO Auto-generated method stub
+		System.out.println(todoItem.toString());
 		
 	}
+
+
+
+	@Override
+	public void displayEventFeedBack(EventItem event) {
+		System.out.println(event.toFileString());
+		
+	}
+
+
+
+	@Override
+	public void displayEventFile(EventItem event) {
+		System.out.println(event.toString());
+		
+	}
+
+
+
+	@Override
+	public void displayTaskFeedBack(TaskItem task) {
+		
+		System.out.println(task.getFeedbackString());	
+	}
+
+
+
+	@Override
+	public void displayTaskFile(TaskItem taskItem) {
+		
+		System.out.println(taskItem.getFeedbackString());	
+	}
+
+
+
+	@Override
+	public void displayToDoFeedBack(ToDoItem todoItem) {
+		System.out.println(todoItem.getFeedbackString());
+		
+	}
+
+	@Override
+	public void displayToDoFile(ToDoItem todoItem) {
+		System.out.println(todoItem.toFileString());
+		
+	}
+	
 	@Override
 	public void printPrompt(String promtMessage) {
 		// TODO Auto-generated method stub
 		
 	}
+	
 	@Override
 	public void displayHelpPage() {
-		// TODO Auto-generated method stub
+		System.out.println("Help Page");
 		
 	}
-	
-	
 	
 	
 }
