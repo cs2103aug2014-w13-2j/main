3c1d0938b0ab0f82dd693baca166c954da9822f1
Vio8023 <Vio@Hus-MacBook-Pro-2.local>
2014-09-30 19:05:05 +0800

    printFeedBack printFile Added

--- src/edu/dynamic/dynamiz/displayer/DisplayerInterface.java
+++ src/edu/dynamic/dynamiz/displayer/DisplayerInterface.java
@@ -14,14 +14,22 @@ public interface DisplayerInterface {
 	public void displayTaskList(ArrayList<TaskItem> taskList);
 	public void displayTaskList(TaskItem[] taskList);
 	public void displayTaskItem(TaskItem task);
+	public void displayTaskFeedBack(TaskItem task);
+	public void displayTaskFile(TaskItem task);
 	
 	public void displayEventList(ArrayList<EventItem> eventList);
 	public void displayEventList(EventItem[] eventList);
 	public void displayEventItem(EventItem event);
+	public void displayEventFeedBack(EventItem event);
+	public void displayEventFile(EventItem event);
+	
 	
 	public void displayToDoList(ArrayList<ToDoItem> todoList);
 	public void displayToDoList(ToDoItem[] todoList);
 	public void displayToDoItem(ToDoItem todoItem);
+	public void displayToDoFeedBack(ToDoItem todoItem);
+	public void displayToDoFile(ToDoItem todoItem);
+	
 	
 	public void printPrompt(String promtMessage);
 
