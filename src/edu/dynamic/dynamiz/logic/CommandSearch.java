793d63342b9b0c1c6daebe62cdd6a8302c7c31e1
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-10-01 14:18:40 +0800

    Added dummy class for each type of Command

---
+++ src/edu/dynamic/dynamiz/logic/CommandSearch.java
@@ -0,0 +1,35 @@
+package edu.dynamic.dynamiz.logic;
+
+public class CommandSearch implements Command {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void redo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
