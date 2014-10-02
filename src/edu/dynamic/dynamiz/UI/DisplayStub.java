bae2d1015fe799c9511294a7e450541f7d4e231c
Vio8023 <Vio@r-104-179-28-172.comp.nus.edu.sg>
2014-10-02 12:27:04 +0800

    displayer stub modified

--- src/edu/dynamic/dynamiz/UI/DisplayStub.java
+++ src/edu/dynamic/dynamiz/UI/DisplayStub.java
@@ -3,26 +3,26 @@ package edu.dynamic.dynamiz.UI;
 import edu.dynamic.dynamiz.structure.ToDoItem;
 
 public class DisplayStub extends Displayer {
-//    @Override
-//    public void printWelcomeMessage(){
-//	super.printWelcomeMessage();
-//	System.out.println("Enter \"add [new task]\", replacing \"[new task]\" with" +
-//		"your desired task description, to begin.");
-//    }
-//    
-//    public void printFeedbackMessage(String message){
-//	System.out.println(message);
-//	System.out.println();
-//    }
-//    
-//    public void printCommandPrompt(){
-//	System.out.print("Command: ");
-//    }
-//    
-//    public static void displayTasks(ToDoItem[] list){
-//	int size = list.length;
-//	for(int i=0; i<size; i++)
-//	    System.out.println(list[i]);
-//	System.out.println();
-//    }
+   // @Override
+    public void WelcomeMessage(){
+	System.out.println(super.displayWelcomeMessage());
+	System.out.println("Enter \"add [new task]\", replacing \"[new task]\" with" +
+		"your desired task description, to begin.");
+    }
+    
+    public void printFeedbackMessage(String message){
+	System.out.println(message);
+	System.out.println();
+    }
+    
+    public void printCommandPrompt(){
+	System.out.print("Command: ");
+    }
+    
+    public static void displayTasks(ToDoItem[] list){
+	int size = list.length;
+	for(int i=0; i<size; i++)
+	    System.out.println(list[i]);
+	System.out.println();
+    }
 }
