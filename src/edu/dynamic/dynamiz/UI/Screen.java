c841f8a77dd5173b33188353f68304cdfe94b013
XYLau <a0114573@A0114573.stu.nus.edu.sg>
2014-10-03 14:59:28 +0800

    Setup displayln function to display a string

--- src/edu/dynamic/dynamiz/UI/Screen.java
+++ src/edu/dynamic/dynamiz/UI/Screen.java
@@ -1,4 +1,5 @@
 package edu.dynamic.dynamiz.UI;
+
 /**
  * @author XYLau
  * 
@@ -10,68 +11,78 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Screen extends JPanel implements ActionListener {
-    protected JTextField inputScreen;
-    protected JTextArea displayScreen;
-    private final static String newline = "\n";
+	protected JTextField inputScreen;
+	protected JTextArea displayScreen;
+	private final static String newline = "\n";
 
-    public Screen() {
-        super(new GridBagLayout());
-        displayScreen = new JTextArea(40, 100);
-        displayScreen.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(displayScreen);
+	public Screen() {
+		super(new GridBagLayout());
+		displayScreen = new JTextArea(40, 100);
+		displayScreen.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(displayScreen);
 
-        inputScreen = new JTextField(20);
-        inputScreen.addActionListener(this);
+		inputScreen = new JTextField(20);
+		inputScreen.addActionListener(this);
 
-        //Add Components to this panel.
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = GridBagConstraints.REMAINDER;
- 
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        add(scrollPane, c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        add(inputScreen, c);
-    }
+		// Add Components to this panel.
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridwidth = GridBagConstraints.REMAINDER;
 
-    public void actionPerformed(ActionEvent evt) {
-        String text = inputScreen.getText();
-        
-        displayScreen.append(text + newline);
-        inputScreen.selectAll();
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		add(scrollPane, c);
 
-        //Make sure the new text is visible, even if there
-        //was a selection in the text area.
-        displayScreen.setCaretPosition(displayScreen.getDocument().getLength());
-    }
+		c.fill = GridBagConstraints.HORIZONTAL;
+		add(inputScreen, c);
+	}
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void Screen() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Dynamiz");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/**
+	 * Event Handler for each event, where event refers to the entry of a single
+	 * command into the Screen interface
+	 */
+	public void actionPerformed(ActionEvent evt) {
+		String text = inputScreen.getText();
 
-        //Add contents to the window.
-        frame.add(new Screen());
+		displayln(text);
+		inputScreen.selectAll();
 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+		// Make sure the new text is visible, even if there
+		// was a selection in the text area.
+		displayScreen.setCaretPosition(displayScreen.getDocument().getLength());
+	}
+	/**
+	 * Displays a string onto the Screen with newline
+	 * @param text
+	 */
+	public void displayln(String text){
+		displayScreen.append(text + newline);
+	}
 
-    public static void main(String[] args) {
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Screen();
-            }
-        });
-    }
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event dispatch thread.
+	 */
+	private static void Screen() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("Dynamiz");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add contents to the window.
+		frame.add(new Screen());
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Screen();
+			}
+		});
+	}
 }
