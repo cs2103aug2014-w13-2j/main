c53feaa088b23da84e31641b6d07ba07fb365147
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-10-05 22:21:44 +0800

    Added testing Aliases and more commands

--- src/edu/dynamic/dynamiz/parser/ParserTest.java
+++ src/edu/dynamic/dynamiz/parser/ParserTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.dynamic.dynamiz.controller.CommandType;
+
 /**
  * Test program to understand the output of parser.
  * @author zixian
@@ -15,17 +17,41 @@ public class ParserTest {
     public void test() {
 	Parser parser = new Parser();
 	CommandLine cmdLine = parser.parse("add task");
-	assertEquals(" task", cmdLine.getParam());
+	assertEquals("task", cmdLine.getParam());
 	
 	cmdLine = parser.parse("delete A1");
-	assertEquals(" A1", cmdLine.getParam());
+	assertEquals("A1", cmdLine.getParam());
 	
 	cmdLine = parser.parse("update A2 from 27/9/2014");
-	assertEquals(" A2 ", cmdLine.getParam());
+	assertEquals("A2", cmdLine.getParam());
 	
+	cmdLine = parser.parse("Search study from today");
+	assertEquals("study", cmdLine.getParam());
+	
+	cmdLine = parser.parse("Show");
+	assertEquals("", cmdLine.getParam());
 	System.out.println(cmdLine);
 	
 	Options options = cmdLine.getOptions();
     }
     
+    @Test
+    public void testAliases() {
+    	Parser parser = new Parser();
+    	
+    	CommandLine cmdLine = null;
+    	String[] addCommands = {"add sth", "AdD sth", "PUT sth", "put sth", "pUt sth"};
+    	
+    	for (String c: addCommands) {
+    		cmdLine = parser.parse(c);
+    		assertEquals(CommandType.ADD, cmdLine.getCommandType());
+    	}
+    	
+    	String[] delCommands = {"delete A1", "DEL A1", "reMoVe A1"};
+
+    	for (String c: delCommands) {
+    		cmdLine = parser.parse(c);
+    		assertEquals(CommandType.DELETE, cmdLine.getCommandType());
+    	}
+    }
 }
