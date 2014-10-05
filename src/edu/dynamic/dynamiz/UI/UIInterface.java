32b204772a1e0d732f461adc2c8371d3c278e965
XYLau <a0114573@nus.edu.sg>
2014-10-05 08:27:33 +0800

    Integration to Displayer (Failed) Unable to call displayPrompt (Null pointer exception)

---
+++ src/edu/dynamic/dynamiz/UI/UIInterface.java
@@ -0,0 +1,12 @@
+package edu.dynamic.dynamiz.UI;
+
+import java.awt.event.ActionEvent;
+/**
+ * Interface for Screen
+ * @author XYLau
+ */
+public interface UIInterface {
+	public void actionPerformed(ActionEvent evt);
+	public void displayln(String text);
+	public void display(String text);	
+}
