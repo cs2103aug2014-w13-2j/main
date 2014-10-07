185485661cd5277f20af5753c6286aed46a2f814
ZiXian92 <zixian1992@hotmail.com>
2014-10-07 11:07:58 +0800

    Undoes changes in Dynamiz.java

--- src/main/Dynamiz.java
+++ src/main/Dynamiz.java
@@ -5,7 +5,7 @@ package main;
 import edu.dynamic.dynamiz.UI.*;
 
 public class Dynamiz {
-	public static void main() {
+	public static void main(String[] args) {
 		UI sc = new UI();
 		System.out.println("Launched");
 	}
