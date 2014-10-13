1be154be8c422327fad57784792e0f3b2db7a2da
ZiXian92 <zixian1992@hotmail.com>
2014-10-13 11:38:05 +0800

    Adds CommandRedo class(unimplemented)

---
+++ src/edu/dynamic/dynamiz/controller/CommandRedo.java
@@ -0,0 +1,37 @@
+package edu.dynamic.dynamiz.controller;
+
+import edu.dynamic.dynamiz.structure.ToDoItem;
+
+public class CommandRedo extends Command {
+    
+    @Override
+    public void execute() {
+	// TODO Auto-generated method stub
+	
+    }
+    
+    @Override
+    public void redo() {
+	// TODO Auto-generated method stub
+	
+    }
+    
+    @Override
+    public void undo() {
+	// TODO Auto-generated method stub
+	
+    }
+    
+    @Override
+    public String getCommandName() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+    
+    @Override
+    public ToDoItem[] getAffectedItems() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+    
+}
