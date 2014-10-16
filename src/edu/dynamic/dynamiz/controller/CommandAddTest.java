c3ca3dc66c8e7fc6cde5ddf3a78d88dcdc3f9105
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-10-17 04:07:26 +0800

    Modified Test

--- src/edu/dynamic/dynamiz/controller/CommandAddTest.java
+++ src/edu/dynamic/dynamiz/controller/CommandAddTest.java
@@ -16,17 +16,14 @@ public class CommandAddTest {
     public void test() {
 	Parser parser = Parser.getInstance();
 	
-	CommandLine cmdLine = parser.parse("Add Learn C++");
-	CommandAdd cmd = (CommandAdd) cmdLine.getCommand();//new CommandAdd(cmdLine.getParam(), cmdLine.getOptions());
+	CommandAdd cmd = (CommandAdd) parser.parse("Add Learn C++");
 	// CommandAdd cmd = new CommandAdd(new ToDoItem("Learn C++"));
 	cmd.execute();
 	ToDoItem[] item = cmd.getAffectedItems();
 	assertEquals("item has 1 object", 1, item.length);
 	cmd.undo();
 	
-	cmdLine = parser.parse("add Learn C++ from 31/10/2014 12:00");
-	System.out.println(cmdLine);
-	cmd = (CommandAdd) cmdLine.getCommand();//new CommandAdd(cmdLine.getParam(), cmdLine.getOptions());
+	cmd = (CommandAdd) parser.parse("add Learn C++ from 31/10/2014 12:00");
 	// cmd = new CommandAdd(new EventItem("Learn C++", new DateTime(31, 10, 2014, 12, 0)));
 	cmd.execute();
     }
