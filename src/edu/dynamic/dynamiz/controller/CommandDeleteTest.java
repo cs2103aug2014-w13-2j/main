28275165cad6ba4636f931f0e2eed54adce202ae
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-10-17 18:26:37 +0800

    Removed redundant statements

--- src/edu/dynamic/dynamiz/controller/CommandDeleteTest.java
+++ src/edu/dynamic/dynamiz/controller/CommandDeleteTest.java
@@ -12,8 +12,7 @@ public class CommandDeleteTest {
     @Test
     public void test() {
 	Parser parser = Parser.getInstance();
-	//CommandDelete cmd = parser.parse("delete A1");
-	CommandDelete cmd = new CommandDelete("A1");
+	CommandDelete cmd = (CommandDelete) parser.parse("delete A1");
 	cmd.execute();
 	assertEquals("A1", cmd.getAffectedItems()[0].getId());
 	cmd.undo();
