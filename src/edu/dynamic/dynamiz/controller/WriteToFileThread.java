b6b51696e516363cf40cbba99937b5e30612dc4f
ZiXian92 <zixian1992@hotmail.com>
2014-10-21 15:23:47 +0800

    Documents WriteToFileThread

--- src/edu/dynamic/dynamiz/controller/WriteToFileThread.java
+++ src/edu/dynamic/dynamiz/controller/WriteToFileThread.java
@@ -5,21 +5,50 @@ import java.util.LinkedList;
 
 import edu.dynamic.dynamiz.structure.ToDoItem;
 
+/**
+ * Defines each thread that writes the given list to the specified file.
+ * 
+ * Constructor
+ * WriteToFileThread(String[] list, String filename)	//Creates a new instance of this thread.
+ * WriteToFileThread(ToDoItem[] list, String filename)	//Creates a new thread that writes the given ToDoItem list to file.
+ * 
+ * Public Methods
+ * void run()	//Executes this thread.
+ * 
+ * @author zixian
+ */
 public class WriteToFileThread extends Thread {
+    //Tracks the threads that were previously executed and to be terminated by the incoming thread.
     private static LinkedList<WriteToFileThread> todoListThreads = new LinkedList<WriteToFileThread>();
     private static LinkedList<WriteToFileThread> completedListThreads = new LinkedList<WriteToFileThread>();
     
-    private boolean isCompletedList = false;
-    private String[] list;
-    private String filename;
+    //Main data members
+    private boolean isCompletedList = false;	//Indicates if this thread is writing the list of completed items to file
+    private String[] list;	//The list of items to write to file
+    private String filename;	//The name of the file to write to
     
+    /**
+     * Creates a new thread to write the given list to file.
+     * This constructor implicitly implies that the given list is a list of ToDoItem objects that are
+     * marked as completed.
+     * @param list The list to be written to file.
+     * @param filename The name of the file to write to.
+     */
     public WriteToFileThread(String[] list, String filename){
+	assert list!=null && filename!=null && !filename.isEmpty();
 	this.list = list;
 	this.filename = filename;
 	this.isCompletedList = true;
     }
     
+    /**
+     * Creates a new thread that writes the file string representation of the items in the given list
+     * to the specified file.
+     * @param list The list of ToDoItem objects to be written to file.
+     * @param filename The name of the file to write to.
+     */
     public WriteToFileThread(ToDoItem[] list, String filename){
+	assert list!=null && filename!=null && !filename.isEmpty();
 	this.list = new String[list.length];
 	int size = 0;
 	for(ToDoItem i: list){
@@ -29,6 +58,9 @@ public class WriteToFileThread extends Thread {
     }
     
     @Override
+    /**
+     * Executes this thread.
+     */
     public void run(){
 	try{
 	    if(isCompletedList){
