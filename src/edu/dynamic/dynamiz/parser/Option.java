f7f5a698dd80164288f3f17b59b9c3241fcae391
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-10-23 08:46:17 +0800

    Deleted redundant OptionInterface

--- src/edu/dynamic/dynamiz/parser/Option.java
+++ src/edu/dynamic/dynamiz/parser/Option.java
@@ -25,7 +25,7 @@ import java.util.Set;
  * Modelled after CLI library
  */
 
-public class Option implements OptionInterface{
+public class Option{
 	/*=========================================================================
 	 * Constants
 	 * ========================================================================
