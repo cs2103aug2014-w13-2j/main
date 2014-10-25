6f66ca576d4e3e3074f439a9c3fddd044c35f96c
XYLau <lauxingyi@hotmail.com>
2014-10-25 20:34:18 +0800

    Create output object

--- src/edu/dynamic/dynamiz/UI/GUI.java
+++ src/edu/dynamic/dynamiz/UI/GUI.java
@@ -2,27 +2,28 @@ package edu.dynamic.dynamiz.UI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextAreaBuilder;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-
+	public static DisplayFormatter disp = new DisplayFormatter();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		BorderPane bp = new  BorderPane();
-	
-		Text t = new Text("Welcome to Dynamiz!");
-		t.setFont(Font.font("Arial", 60));
-		t.setEffect(new DropShadow(2,3,3,Color.BLUE));
-		
-		
-		bp.setCenter(t);
-			
 		primaryStage.setTitle("Dynamiz");
+		
+		// Set output area
+		TextArea ta = TextAreaBuilder.create().prefWidth(800).prefHeight(600).wrapText(true).build();
+		ta.setEditable(false);
+		bp.setCenter(ta);
+		
+		ta.setText(disp.displayWelcomeMessage()+"\n");
+		ta.appendText(disp.displayPrompt(1)+"\n");
 		
 		primaryStage.setScene(new Scene(bp));
 		primaryStage.show();
