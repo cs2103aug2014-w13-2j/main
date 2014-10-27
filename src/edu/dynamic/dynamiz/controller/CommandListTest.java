9c264612f8543b5a1f1ddb5f57b2b2e63a6fd611
ZiXian92 <zixian1992@hotmail.com>
2014-10-27 18:51:22 +0800

    Tests the new Storage implementation for CommandList

--- src/edu/dynamic/dynamiz/controller/CommandListTest.java
+++ src/edu/dynamic/dynamiz/controller/CommandListTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import edu.dynamic.dynamiz.parser.OptionType;
 import edu.dynamic.dynamiz.parser.Parser;
+import edu.dynamic.dynamiz.structure.MyDate;
 import edu.dynamic.dynamiz.structure.ToDoItem;
 
 /**
@@ -20,7 +21,7 @@ public class CommandListTest {
 	OptionType[] optList = new OptionType[2];
 	optList[0] = OptionType.PRIORITY;
 	optList[1] = OptionType.START_TIME;
-	Command cmd = new CommandList(optList);
+	Command cmd = new CommandList(null, null, null, optList);
 	cmd.execute();
 	ToDoItem[] list = cmd.getAffectedItems();
 	for(ToDoItem i: list)
@@ -29,9 +30,25 @@ public class CommandListTest {
 	
 	optList = new OptionType[1];
 	optList[0] = OptionType.END_TIME;
-	cmd = new CommandList(optList);
+	cmd = new CommandList(null, null, null, optList);
 	cmd.execute();
 	list = cmd.getAffectedItems();
+	for(ToDoItem i: list)
+	    System.out.println(i);
+	System.out.println();
+    }
+    
+    @Test
+    public void testFilter(){
+	int[] pri = new int[2];
+	pri[0] = 2;
+	pri[1] = 4;
+	MyDate[] end = new MyDate[2];
+	end[0] = new MyDate(31, 10, 2014);
+	end[1] = new MyDate(1, 11, 2014);
+	Command cmd = new CommandList(pri, null, end, null);
+	cmd.execute();
+	ToDoItem[] list = cmd.getAffectedItems();
 	for(ToDoItem i: list)
 	    System.out.println(i);
 	System.out.println();
