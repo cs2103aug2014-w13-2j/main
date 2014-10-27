1f6c5ed3bc4f93f9f6a2622f7eae4ae29145b01f
XYLau <lauxingyi@hotmail.com>
2014-10-27 17:16:07 +0800

    Update details & comments

--- src/edu/dynamic/dynamiz/UI/ScreenInterface.java
+++
@@ -1,9 +0,0 @@
-package edu.dynamic.dynamiz.UI;
-
-import java.awt.event.ActionEvent;
-
-public interface ScreenInterface {
-	public void actionPerformed(ActionEvent evt);
-	public void displayln(String text);
-	public void display(String text);	
-}
