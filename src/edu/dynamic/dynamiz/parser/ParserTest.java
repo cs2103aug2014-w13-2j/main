ca367fb20a4cb94d14629d2c9d8a55c81a8e82e0
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-11-01 06:42:01 +0800

    Added a few partial test for the implementation of heuristic parsing

--- src/edu/dynamic/dynamiz/parser/ParserTest.java
+++ src/edu/dynamic/dynamiz/parser/ParserTest.java
@@ -11,6 +11,7 @@ import edu.dynamic.dynamiz.structure.MyDate;
 /**
  * Test program to understand the output of parser.
  * @author zixian
+ * @author nhan
  */
 
 public class ParserTest {
@@ -21,11 +22,11 @@ public class ParserTest {
 		CommandLine cmdLine = parser.parseCommandLine("add task");
 		assertEquals("task", cmdLine.getParam());
 
-		cmdLine = parser.parseCommandLine("delete A1");
-		assertEquals("A1", cmdLine.getParam());
+		cmdLine = parser.parseCommandLine("delete 1");
+		assertEquals("1", cmdLine.getParam());
 
-		cmdLine = parser.parseCommandLine("add A2 on 27/9/2014 from yesterday 18:00 to 23:00");
-		assertEquals("A2", cmdLine.getParam());
+		cmdLine = parser.parseCommandLine("add 2 on 27/9/2014 from yesterday 18:00 to 23:00");
+		assertEquals("2", cmdLine.getParam());
 
 		//cmdLine = parser.parseCommandLine("Search study from today");
 		//assertEquals("study", cmdLine.getParam());
@@ -33,8 +34,19 @@ public class ParserTest {
 		cmdLine = parser.parseCommandLine("list -s tomorrow,today,yesterday orderby -s,to,priority");
 		assertEquals("", cmdLine.getParam());
 		
-		cmdLine = parser.parseCommandLine("update A3 priority 3 on tomorrow from 18:00 to 23:00");
-		System.out.println(cmdLine);
+		cmdLine = parser.parseCommandLine("update 3 priority h on tomorrow from 18:00 to 23:00");
+    }
+    
+    @Test
+    public void testMultipleOption() {
+    	Parser parser = Parser.getInstance();
+    	CommandLine cmdLine;
+    	
+    	cmdLine = parser.parseCommandLine("add a task from today from yesterday from tomorrow");
+    	assertEquals("a task from today from yesterday", cmdLine.getParam());
+    	
+    	cmdLine = parser.parseCommandLine("add a task from today from yesterday to tomorrow");
+    	assertEquals("a task from today", cmdLine.getParam());
     }
     
     @Test
@@ -68,8 +80,6 @@ public class ParserTest {
     	assertEquals("31/10/2014 16:00", date.toString());
     	
     	date = parser.parseDate("yesterday 18:00");
-    	System.out.println(date.toString());
-    	
     }
     
     @Test
