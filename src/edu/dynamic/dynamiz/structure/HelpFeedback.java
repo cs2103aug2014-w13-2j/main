cbad6a33c56ec3989912f4faee02d22e51d21afc
ZiXian92 <zixian1992@hotmail.com>
2014-11-01 21:44:21 +0800

    Documents HelpFeedback class

--- src/edu/dynamic/dynamiz/structure/HelpFeedback.java
+++ src/edu/dynamic/dynamiz/structure/HelpFeedback.java
@@ -2,6 +2,14 @@ package edu.dynamic.dynamiz.structure;
 
 /**
  * Defines the feedback type for help command.
+ * 
+ * Constructor
+ * HelpFeedback(String commandType, String command, String content)	//Creates a new instance of this feedback.
+ * 
+ * Public Methods
+ * String getHelpContent()	//Gets the help page content.
+ * String getClassName()	//Gets the String representation of this feedback's class.
+ * 
  * @author zixian
  */
 public class HelpFeedback extends Feedback {
@@ -10,7 +18,12 @@ public class HelpFeedback extends Feedback {
     //Main data member
     private String helpContents;
     
-    //Constructor
+    /**
+     * Creates a new instance of this feedback.
+     * @param commandType the type of command this feedback is used for.
+     * @param command The input command this feedback is used for.
+     * @param content The help page content to be displayed.
+     */
     public HelpFeedback(String commandType, String command, String content){
 	super(commandType, command);
 	setContent(content);
@@ -30,6 +43,10 @@ public class HelpFeedback extends Feedback {
     }
     
     @Override
+    /**
+     * Gets the String representation of this feedback's class.
+     * @return The String representation of this feedback's class.
+     */
     public String getClassName(){
 	return CLASSNAME;
     }
