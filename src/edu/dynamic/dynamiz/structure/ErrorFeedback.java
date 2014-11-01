9ab59dfe4d7d1f274f19bdbb48e1f986cd1f4e35
ZiXian92 <zixian1992@hotmail.com>
2014-11-01 21:34:46 +0800

    Documents ErrorFeedback class

--- src/edu/dynamic/dynamiz/structure/ErrorFeedback.java
+++ src/edu/dynamic/dynamiz/structure/ErrorFeedback.java
@@ -1,12 +1,30 @@
 package edu.dynamic.dynamiz.structure;
 
+/**
+ * Defines the feedback class that is used when errors occurred during execution.
+ * 
+ * Constructor
+ * ErrorFeedback(String commandType, String command, String message)	//Creates a new instance of this feedback.
+ * 
+ * Public Methods
+ * String getMessage()	//Gets the error message of this feedback.
+ * String getClassName()	//Gets the String representation of this feedback's class.
+ * 
+ * @author zixian
+ */
 public class ErrorFeedback extends Feedback {
+    //The String representation of this feedback's class.
     public static final String CLASSNAME = "ErrorFeedback";
     
     //Main data member
     private String message;
     
-    //Constructor
+    /**
+     * Creates a new instance of this feedback.
+     * @param commandType The type of command that caused an error.
+     * @param command The input command string that caused the error.
+     * @param message The error message to be shown to user.
+     */
     public ErrorFeedback(String commandType, String command, String message){
 	super(commandType, command);
 	setMessage(message);
@@ -26,6 +44,10 @@ public class ErrorFeedback extends Feedback {
     }
     
     @Override
+    /**
+     * gets the String representation of this feedback's class.
+     * @return The String representation of this feedback's class.
+     */
     public String getClassName(){
 	return CLASSNAME;
     }
