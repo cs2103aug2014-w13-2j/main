9fbdf1b3b9c7cd72376a3a3189cdc50d2c1c7b85
ZiXian92 <zixian1992@hotmail.com>
2014-11-01 22:27:50 +0800

    Documents SuccessFeedback class

--- src/edu/dynamic/dynamiz/structure/SuccessFeedback.java
+++ src/edu/dynamic/dynamiz/structure/SuccessFeedback.java
@@ -2,6 +2,14 @@ package edu.dynamic.dynamiz.structure;
 
 /**
  * Defines the feedback object type for successful operations, excluding the help page command.
+ * 
+ * Constructor
+ * SuccessFeedback(String commandType, String command, ToDoItem[] list)	//Creates a new instance of this feedback.
+ * 
+ * Public Methods
+ * ToDoItem[] getAffectedItems()	//Gets the list of ToDoItem objects affected by the command this feedback is used for.
+ * String getClassName()	//Gets the String representation of this feedback's class.
+ * 
  * @author zixian
  */
 public class SuccessFeedback extends Feedback {
@@ -10,10 +18,15 @@ public class SuccessFeedback extends Feedback {
     //Main data members
     private ToDoItem[] list;
     
-    //Constructor
+    /**
+     * Creates a new instance of this feedback.
+     * @param commandType The command type this feedback is used for.
+     * @param command The input command this feedback is used for.
+     * @param list The list of ToDoItem objects affected by the command.
+     */
     public SuccessFeedback(String commandType, String command, ToDoItem[] list){
 	super(commandType, command);
-	setList(list);
+	this.list = list;
     }
     
     /**
@@ -25,11 +38,11 @@ public class SuccessFeedback extends Feedback {
 	return list;
     }
     
-    private void setList(ToDoItem[] list){
-	this.list = list;
-    }
-    
     @Override
+    /**
+     * Gets the String representation of this feedback's class.
+     * @return the String representation of this feedback's class.
+     */
     public String getClassName(){
 	return CLASSNAME;
     }
