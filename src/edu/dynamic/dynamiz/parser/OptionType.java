b144cd04bd8dae926cf0a18f3894d35f041e8595
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-11-06 02:47:40 +0800

    Sanitised Code by removing irrelevant import

--- src/edu/dynamic/dynamiz/parser/OptionType.java
+++ src/edu/dynamic/dynamiz/parser/OptionType.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import net.fortuna.ical4j.model.property.StatusFactory;
-
 /**
  * OptionType enum class is a class to hold the various options applicable to many commands.
  * Using OptionType allows the users to specify the PRIORITY, START_TIME, END_TIME, or
