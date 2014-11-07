1626fd3556683dc28f3147fbaa817d2e79f58ecb
ZiXian92 <zixian1992@hotmail.com>
2014-11-07 16:38:57 +0800

    Touches up CommandAddTest

--- src/edu/dynamic/dynamiz/controller/CommandAddTest.java
+++ src/edu/dynamic/dynamiz/controller/CommandAddTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 
 import edu.dynamic.dynamiz.parser.CommandLine;
 import edu.dynamic.dynamiz.parser.Parser;
+import edu.dynamic.dynamiz.structure.MyDate;
 import edu.dynamic.dynamiz.structure.MyDateTime;
 import edu.dynamic.dynamiz.structure.EventItem;
+import edu.dynamic.dynamiz.structure.TaskItem;
 import edu.dynamic.dynamiz.structure.ToDoItem;
 
 /**
@@ -23,18 +25,17 @@ public class CommandAddTest {
 	cmd.execute();
 	assertEquals(1, cmd.getAffectedItems().length);
 	assertEquals("Learn C++", cmd.getAffectedItems()[0].getDescription());
-	//((Undoable)cmd).undo();
+	((Undoable)cmd).undo();	//To clean up the todo.txt
 	
 	cmd = new CommandAdd(new EventItem("Learn C++", new MyDateTime(31, 10, 2014, 12, 0), new MyDateTime(11, 11, 2014, 12, 0)));
 	cmd.execute();
 	assertEquals(new MyDateTime(31, 10, 2014, 12, 0), ((EventItem)cmd.getAffectedItems()[0]).getStartDate());
 	assertEquals(new MyDateTime(11, 11, 2014, 12, 0), ((EventItem)cmd.getAffectedItems()[0]).getEndDate());
-	//((Undoable)cmd).undo();
-    }
-    
-    // TODO: Implement Exception for CommandAdd
-    @Test(expected=IllegalArgumentException.class)
-    public void testIllegalActions(){
-	Parser parser = Parser.getInstance();
+	((Undoable)cmd).undo();	//To clean up todo.txt
+	
+	cmd = new CommandAdd(new TaskItem("PS7R", new MyDate(13, 11, 2014)));
+	cmd.execute();
+	assertEquals(new MyDate(13, 11, 2014), ((TaskItem)cmd.getAffectedItems()[0]).getDeadline());
+	((Undoable)cmd).undo();
     }
 }
