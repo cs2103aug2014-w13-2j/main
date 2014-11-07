4cf342d2c4504807a1259bb70610d167e0d0be1d
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-11-07 22:40:52 +0800

    Turned off Logger

--- src/main/Dynamiz.java
+++ src/main/Dynamiz.java
@@ -1,4 +1,6 @@
 package main;
+import java.util.logging.LogManager;
+
 /**
  * Main Driver Program for Dynamiz. Execute Dynmaiz from here.
  * @author A0114573J
@@ -7,6 +9,8 @@ import edu.dynamic.dynamiz.UI.*;
 
 public class Dynamiz {
 	public static void main(String[] args) {
+		// Turn of Logger
+		LogManager.getLogManager().reset();
 		UI ui = new UI();
 		ui.run();
 	}
